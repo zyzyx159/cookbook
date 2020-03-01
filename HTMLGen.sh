92a2819ef3e033f326db896997b81389271f6c6a
Daniel Stellmon <dstellm@gmail.com>
2020-02-29 23:34:55 -0600

    wrote HTMLGen, added link to index on all recipies

---
+++ HTMLGen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#for f in $PWD/*.adoc
+#do
+#    asciidoctor -a linkcss -a nofooter $f
+#done
+
+OUT="~/AsciiDoctor/CookBook"
+
+rm $OUT/*
+
+#echo $OUT/*
+
+find . -name "*.adoc" -print0 | while read -d $'\0' file
+do
+    asciidoctor -a linkcss -a nofooter -D $OUT $file
+done
