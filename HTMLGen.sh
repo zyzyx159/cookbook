a437ca526a9083c2eca71f77b4ef4f2eb43e8c3e
Daniel Stellmon <dstellm@gmail.com>
2020-03-01 01:05:12 -0600

    htmlgen clean up

--- HTMLGen.sh
+++ HTMLGen.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+# asciidoctor = call the program
+# -r asciidoctor-html5s -b html5s = use the custom conversion (seperate download) 
+#   gem install asciidoctor-html5s
+# -a no-header 
+# -a no-footer
+# -a linkcss = don't dump the css text into my HTML
+# -a stylesheet=<stylesheet.css> = what CSS file to use
+# -a stylesdir=<location> = folder where the CSS is stored
+#	default is /usr/lib/ruby/gems/2.7.0/gems/asciidoctor-2.0.10/data/stylesheets
+# -D output folder
 
-#for f in $PWD/*.adoc
-#do
-#    asciidoctor -a linkcss -a nofooter $f
-#done
+OUT="${PWD}/HTML"
+# ${PWD} = current working directory
 
-OUT="HTML"
+mkdir -p $output
+# if the HTML folder does not exist make one
 
 rm $OUT/*
-
-#echo $OUT/*
+# delete every thing in it
 
 find . -name "*.adoc" -print0 | while read -d $'\0' file
 do
-    asciidoctor -a linkcss -a nofooter -D $OUT $file
+    asciidoctor -r asciidoctor-html5s -b html5s -a no-header -a no-footer -a linkcss -D $OUT $file
 done
